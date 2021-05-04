92ff5607274497c7a6e3c26c5ddf2e42c0bb2b76
0Nera <slizen.can@gmail.com>
2021-05-04 18:05:52 +0300

    0.1

--- upload.bat
+++ upload.bat
@@ -1,7 +1,7 @@
 git add .
 git remote -v
 git status
-git tag -a v1.0 -m "Version 0.1"
+git tag -a v0.1 -m "Version 0.1"
 git commit -m "0.1"
 git remote add origin https://github.com/0Nera/KolibriRPG.git
 git pull origin master
